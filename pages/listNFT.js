56bb0dbafae270bf09a4cbd01f4d57dedecbdd02
Adarsh-K <adarsh18213@iiitd.ac.in>
2022-07-01 22:44:33 +0530

    des: Added image upload placeholder on left side

--- pages/listNFT.js
+++ pages/listNFT.js
@@ -68,38 +68,52 @@ export default function ListNFT() {
 	}
 
 	return (
-		<div className="flex justify-center">
-			<div className="w-1/2 flex flex-col pb-12">
-				<input 
-					placeholder="Asset Name"
-					className="mt-8 border rounded p-4"
-					onChange={ e => updateFormInput({ ...formInput, name: e.target.value }) }
-				/>
-				<textarea
-					placeholder="Asset Description"
-					className="mt-2 border rounded p-4"
-					onChange={ e => updateFormInput({ ...formInput, description: e.target.value } )}
-				/>
-				<input
-					placeholder="Asset Price in MATIC"
-					className="mt-2 border rounded p-4"
-					onChange={ e => updateFormInput({ ...formInput, price: e.target.value }) }
-				/>
-				<input
-					type="file"
-					name="Asset"
-					className="my-4"
-					onChange={ onChange }
-				/>
-				{
-				fileUrl && (
-					<img className="rounded mt-4" width="350" src={fileUrl} />
-				)
-				}
-				<button onClick={ listNFT } className="font-bold mt-4 bg-pink-500 text-white rounded p-4 shadow-lg">
-					List NFT
-				</button>
-			</div>
-		</div>
+    <div className="grid grid-cols-2">
+      {fileUrl ? (
+        <img className="rounded m-4 w-1/2 h-full ml-60" src={fileUrl} />
+      ) : (
+        <div class="m-4 w-1/2 h-full ml-60">
+          <div class="flex justify-center h-2/3">
+            <label class="flex flex-col w-full h-full border-4 justify-center border-dashed hover:bg-gray-100 hover:border-gray-300">
+                <div class="flex flex-col items-center justify-center pt-7">
+                    <svg xmlns="http://www.w3.org/2000/svg"
+                        class="w-12 h-12 text-gray-400 group-hover:text-gray-600" viewBox="0 0 20 20"
+                        fill="currentColor">
+                        <path fill-rule="evenodd"
+                            d="M4 3a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V5a2 2 0 00-2-2H4zm12 12H4l4-8 3 6 2-4 3 6z"
+                            clip-rule="evenodd" />
+                    </svg>
+                    <p class="pt-1 text-sm tracking-wider text-gray-400 group-hover:text-gray-600">
+                        Select a photo</p>
+                </div>
+                <input type="file" name="Asset" class="opacity-0" onChange={ onChange } />
+            </label>
+          </div>
+          <label class="block text-center mt-4 text-gray-500">Upload
+              Image (jpg, png, svg, jpeg, gif)</label>
+        </div>
+      )
+      }
+      <div className="w-1/2 flex flex-col ml-4 pb-12">
+        <input 
+          placeholder="NFT Name"
+          className="mt-4 border rounded p-4"
+          onChange={ e => updateFormInput({ ...formInput, name: e.target.value }) }
+        />
+        <textarea
+          placeholder="NFT Description"
+          className="mt-2 border rounded p-4"
+          onChange={ e => updateFormInput({ ...formInput, description: e.target.value } )}
+        />
+        <input
+          placeholder="NFT Price in MATIC"
+          className="mt-2 border rounded p-4"
+          onChange={ e => updateFormInput({ ...formInput, price: e.target.value }) }
+        />
+        <button onClick={ listNFT } className="font-bold mt-4 bg-pink-500 text-white rounded p-4 shadow-lg">
+          List NFT
+        </button>
+      </div>
+    </div>
 	)
 }
